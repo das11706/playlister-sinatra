35cbb75c7c43daf2412887e306e8775d16db423b
Daniel Suarez <suarez.daniel101@gmail.com>
2022-02-08 22:13:12 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,17 @@
 class ArtistsController < ApplicationController
+
+  get '/artists' do 
+    @artists = Artist.all 
+    erb :'/artists/index'
+  end
+ 
+  get '/artists/new' do 
+    @songs = Song.all 
+    erb :'/artists/new'
+  end
+
+  post '/artists' do 
+    @artist = Artist.create(params[:artist])
+  end
+
 end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,18 @@
+class SongsController < ApplicationController 
+
+  get '/songs' do 
+    @songs = Song.all 
+    erb :'/songs/index'
+  end
+
+  get '/songs/new' do 
+    @artists = Artist.all 
+    erb :'/songs/new'
+  end
+
+  post '/songs' do
+    @song = Song.create(params[:song])
+    
+  end
+
+end
