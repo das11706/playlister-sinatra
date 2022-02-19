c21657fee4170c13ce28f8c5156ad4c7c3f07239
Daniel Suarez <suarez.daniel101@gmail.com>
2022-02-18 22:44:26 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,11 @@
 class Genre < ActiveRecord::Base
-  include Slugifiable
-  
+  # include Slugifiable
+
   has_many :artists, through: :songs
   has_many :song_genres
   has_many :songs, through: :song_genres  
+
+  def slug
+    name.gsub(" ", "-")
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,13 +1,9 @@
 class Song < ActiveRecord::Base
   
-
   belongs_to :artist 
   belongs_to :genre
   has_many :song_genres
   has_many :genres, through: :song_genres 
-
-  
-
 
   def slug
     name.gsub(" ", "-")
